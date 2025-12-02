efeae73f0ce8004e8fdfd46d9bc8ee4ced644aa2
Syed <xayanmay@gmail.com>
2025-12-01 21:23:44 -0500

    Modified

--- 1-pure-impure.js
+++ 1-pure-impure.js
@@ -1,9 +1,27 @@
 const nums = [1, 2, 3]
 
+// output = [2, 4, 6]
+
 const doublePure = (arr) => {
-  
+    // made a new array
+    const newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr[i] = arr[i] * 2
+
+    }
+    return newArr
+
 };
 
+console.log(doublePure(nums));
+
+
 const doubleImpure = (arr) => {
-  
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i] * 2
+    }
+    return arr
 };
+
+console.log(doubleImpure(nums))
